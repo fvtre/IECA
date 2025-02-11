da902d3c3b696179118127f926ccd0fcd969140b
fvtre <ad.lecaros@duocuc.cl>
2025-02-10 23:21:30 -0300

    card update

--- IECA/urls.py
+++ IECA/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('signup/', views.signup,name='signup'),
     path('tasks/', views.tasks,name='tasks'),
     path('logout/', views.signout,name='logout'),
